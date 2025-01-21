af0ad6cbfc0cd209fa0cf560deae66dcc3faf297
Sos <sos.petrosyan@betconstruct.com>
2025-01-21 15:38:34 +0400

    fix(SplitButton): changed structure of split button

--- src/lib/atoms/SplitButton/SplitButton.tsx
+++ src/lib/atoms/SplitButton/SplitButton.tsx
@@ -3,6 +3,7 @@ import React, { forwardRef } from 'react';
 // Styles
 import './SplitButton.scss';
 import { Icon } from '../../../index';
+import { ArrowBigRight, ArrowRight } from 'lucide-react';
 
 interface ISplitButtonProps {
     /**
@@ -16,60 +17,13 @@ interface ISplitButtonProps {
  */
 const SplitButton = forwardRef<unknown, ISplitButtonProps>(({ size }: ISplitButtonProps, ref) => {
     return (
-        <div className="splitButtonTestHolder">
-            <div className="splitButton">
+        <div className="splitButton">
+            <div className="splitButton__content">
                 <button className="splitButton__button splitButton__button_size_large splitButton__button_color_primary splitButton__button_type_fill splitButton__button_icon_before">
                     <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
                     <span className="splitButton__text">Button</span>
                 </button>
                 <button className="splitButton__button splitButton__button_size_large splitButton__button_color_primary splitButton__button_type_fill splitButton__button_icon_only">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                </button>
-            </div>
-            <div className="splitButton">
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_primary splitButton__button_type_outline splitButton__button_icon_before">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                    <span className="splitButton__text">Button</span>
-                </button>
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_primary splitButton__button_type_outline splitButton__button_icon_only">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                </button>
-            </div>
-
-            <div className="splitButton">
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_secondary splitButton__button_type_fill splitButton__button_icon_before">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                    <span className="splitButton__text">Button</span>
-                </button>
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_secondary splitButton__button_type_fill splitButton__button_icon_only">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                </button>
-            </div>
-            <div className="splitButton">
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_secondary splitButton__button_type_outline splitButton__button_icon_before">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                    <span className="splitButton__text">Button</span>
-                </button>
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_secondary splitButton__button_type_outline splitButton__button_icon_only">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                </button>
-            </div>
-
-            <div className="splitButton">
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_inverse splitButton__button_type_fill splitButton__button_icon_before">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                    <span className="splitButton__text">Button</span>
-                </button>
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_inverse splitButton__button_type_fill splitButton__button_icon_only">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                </button>
-            </div>
-            <div className="splitButton">
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_inverse splitButton__button_type_outline splitButton__button_icon_before">
-                    <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
-                    <span className="splitButton__text">Button</span>
-                </button>
-                <button className="splitButton__button splitButton__button_size_large splitButton__button_color_inverse splitButton__button_type_outline splitButton__button_icon_only">
                     <Icon className="splitButton__icon" type={'bc-icon-info'} disabled={false} isFilled={true} />
                 </button>
             </div>
