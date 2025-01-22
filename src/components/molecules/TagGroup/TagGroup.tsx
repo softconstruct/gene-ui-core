6eca07ecdf1c41076d8d0e0626b39cc459290892
abrahamyan.karen <abrahamyan.karen@softconstruct.com>
2025-01-22 16:47:36 +0400

    feat(TagGroup): component UI styles

--- src/components/molecules/TagGroup/TagGroup.tsx
+++ src/components/molecules/TagGroup/TagGroup.tsx
@@ -1,7 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import classNames from "classnames";
 // Styles
 import "./TagGroup.scss";
+import { ChevronDown, ChevronUp } from "@geneui/icons";
+import Tag from "../Tag";
+import { Button } from "../../../index";
 
 interface ITagGroupProps {
     /**
@@ -16,7 +19,57 @@ interface ITagGroupProps {
  * Tag Group displays a list of selected options, offering a clear and organized way to present chosen items.
  */
 const TagGroup: FC<ITagGroupProps> = ({ className }) => {
-    return <div className={classNames("tagGroup", className)}>TagGroup</div>;
+    // todo: refactor functional part if needed
+    const [isExpanded, setIsExpanded] = useState(false);
+    const moreTagsQuantity = 12;
+    const toggleText = () => setIsExpanded((prev) => !prev);
+    const removeTag = () => {
+        console.log("remove tag");
+    };
+
+    return (
+        <div className={classNames(`tagGroup ${isExpanded ? "tagGroup_extended" : ""}`, className)}>
+            <div className="tagGroup__container">
+                <div className="tagGroup__tags">
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                    <Tag text="Tag" onClose={removeTag} />
+                </div>
+            </div>
+            <Button
+                className="tagGroup__showButton"
+                appearance="secondary"
+                size="small"
+                displayType="text"
+                iconAfter
+                Icon={isExpanded ? ChevronUp : ChevronDown}
+                text={isExpanded ? "Show less" : `Show ${moreTagsQuantity} more`}
+                onClick={toggleText}
+            />
+        </div>
+    );
 };
 
 export { ITagGroupProps, TagGroup as default };
