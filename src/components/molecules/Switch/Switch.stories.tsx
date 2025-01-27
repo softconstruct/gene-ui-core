b179fa8e8b01733b06127918113ffb7f205ce9c8
Arthur Grigoryan <arthur.grigoryan@softconstruct.com>
2025-01-27 18:13:05 +0400

    feat(Switch): added props and joined to css classes

--- src/components/molecules/Switch/Switch.stories.tsx
+++ src/components/molecules/Switch/Switch.stories.tsx
@@ -11,10 +11,20 @@ const meta: Meta<typeof Switch> = {
     title: "Molecules/Switch",
     component: Switch,
     argTypes: {
-        className: args({ control: "false", ...propCategory.appearance })
+        className: args({ control: "false", ...propCategory.appearance }),
+        label: args({ control: "text", ...propCategory.content }),
+        helperText: args({ control: "text", ...propCategory.content }),
+        labelAlignment: args({ control: "select", ...propCategory.appearance }),
+        disabled: args({ control: "boolean", ...propCategory.states }),
+        readonly: args({ control: "boolean", ...propCategory.states })
         // fill Switch component argTypes
     },
     args: {
+        label: "Label",
+        helperText: "Helper Text",
+        disabled: false,
+        readonly: false,
+        labelAlignment: "after"
         // fill Switch component args
     } as ISwitchProps
 };
--- src/components/molecules/Switch/Switch.tsx
+++ src/components/molecules/Switch/Switch.tsx
@@ -6,6 +6,11 @@ import { CheckMark } from "@geneui/icons";
 import HelperText from "../../atoms/HelperText";
 
 interface ISwitchProps {
+    label?: string;
+    helperText?: string;
+    disabled?: boolean;
+    readonly?: boolean;
+    labelAlignment?: "after" | "before" | "top";
     /**
      * Additional class for the parent element.
      * This prop should be used to set placement properties for the element relative to its parent using BEM conventions.
@@ -17,18 +22,27 @@ interface ISwitchProps {
 /**
  * A switch component allows users to toggle between two states, typically "on" and "off". It is commonly used in settings and preferences to enable or disable features or functionalities.
  */
-const Switch: FC<ISwitchProps> = ({ className }) => {
+const Switch: FC<ISwitchProps> = ({ className, label, helperText, disabled, readonly, labelAlignment = "after" }) => {
     return (
-        // todo: switch in the next classNames for labelText alignment: "switch_labelAfter", "switch_labelBefore", "switch_labelTop"
-        <div className={classNames("switch switch_labelAfter", className)}>
+        <div
+            className={classNames(
+                "switch",
+                {
+                    switch_labelAfter: labelAlignment === "after",
+                    switch_labelBefore: labelAlignment === "before",
+                    switch_labelTop: labelAlignment === "top"
+                },
+                className
+            )}
+        >
             <label className="switch__label">
-                <input type="checkbox" className="switch__input" disabled={false} readOnly={false} />
+                <input type="checkbox" className="switch__input" disabled={disabled} readOnly={!disabled && readonly} />
                 <span className="switch__slider">
                     <CheckMark size={16} className="switch__icon" />
                 </span>
-                <span className="switch__labelText">Label</span>
+                {label && <span className="switch__labelText">{label}</span>}
             </label>
-            <HelperText text="Helper Text" className="switch__helperText" />
+            {helperText && <HelperText text={helperText} className="switch__helperText" isDisabled={disabled} />}
         </div>
     );
 };
